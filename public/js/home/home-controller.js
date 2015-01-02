0fce68727652594b8b08a3be93c527da4cd820aa
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-01-02 11:17:24 -0800

    Remove unnecessary log.

--- public/js/home/home-controller.js
+++ public/js/home/home-controller.js
@@ -3,7 +3,6 @@
 angular.module('quotee')
   .controller('HomeController', ['$scope', '$log', 'Quotes', function ($scope, $log, Quotes) {
     Quotes.get(function (quote) {
-      $log.log(quote)
       $scope.quote = quote.body;
       $scope.author = quote.author;
     });
