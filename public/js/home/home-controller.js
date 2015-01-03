3e6764bbea329a77fee5e6da8cb9c608f4b8dc72
Jacob Meacham <jmeacham@palantir.com>
2015-01-02 22:51:44 -0800

    Add inverse view.

--- public/js/home/home-controller.js
+++ public/js/home/home-controller.js
@@ -1,9 +1,13 @@
 'use strict';
 
 angular.module('quotee')
-  .controller('HomeController', ['$scope', '$log', 'Quotes', function ($scope, $log, Quotes) {
+  .controller('HomeController', ['$scope', '$routeParams', 'Quotes', function ($scope, $routeParams, Quotes) {
+    if ($routeParams.style === 'inverse' || Math.random() > 0.5) {
+      $scope.bodyClass = "inverse"
+    }
+
     Quotes.get(function (quote) {
       $scope.quote = quote.body;
-      $scope.author = quote.author;
+      $scope.author = "- " + quote.author;
     });
 }]);
